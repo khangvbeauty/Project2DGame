4fd3d9c199a54195407621c3b44d7d7669898783
Nguyễn Vĩ Khang <68845361+khangvbeauty@users.noreply.github.com>
2023-12-09 17:23:44 +0700

    update

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -129,18 +129,21 @@ public class Player extends Entity { //lớp kế thừa của lớp Entity
 		        
 		        switch(objectName) {
 		        case "Key":
+		        	gp.playSE(1);
 		        	hasKey++;
 		        	gp.obj[i] = null;
 		        	System.out.println("Key: "+hasKey);
 		        	break;
 		        case "Door":
 		        	if(hasKey > 0) {
+		        		gp.playSE(3);
 		        		gp.obj[i] = null;
 		        		hasKey--;
 		        	}
 		        	System.out.println("Key: "+hasKey);
 		        	break;   
 		        case "Boots":
+		        	gp.playSE(2);
 		        	speed += 2;
 		        	gp.obj[i]=null;
 		        	break;
